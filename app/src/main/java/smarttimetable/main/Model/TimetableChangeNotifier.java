6d759f4149a47fbe0ec3a9a4647f20d4a98c3540
Nick <bnxaxnb@gmail.com>
2021-05-05 17:03:26 +0300

    Update timetable change list localization

--- app/src/main/java/smarttimetable/main/Model/TimetableChangeNotifier.java
+++ app/src/main/java/smarttimetable/main/Model/TimetableChangeNotifier.java
@@ -10,6 +10,7 @@ import smarttimetable.main.Model.DBModels.Lesson;
 import smarttimetable.main.Model.DBModels.Subject;
 import smarttimetable.main.Model.DBModels.Teacher;
 import smarttimetable.main.Model.DBModels.Week;
+import smarttimetable.main.R;
 
 public class TimetableChangeNotifier {
     private static ArrayList<String> ChangeList = new ArrayList<>();
@@ -40,10 +41,10 @@ public class TimetableChangeNotifier {
         for (int i = 0; i < cache.size(); i++) {
             if (DataBaseOperation.GetLessonById(cache.get(i).getIdtimetable()) == null)
             {
-                ChangeList.add("Удален урок "+cache.get(i).toString());
+                ChangeList.add(R.string.ChangeList_LessonDelete + cache.get(i).toString());
             }else if(!DataBaseOperation.GetLessonById(cache.get(i).getIdtimetable()).equals(cache.get(i)))
             {
-                ChangeList.add("Изменен урок "+cache.get(i).toString());
+                ChangeList.add(R.string.ChangeList_LessonUpdate+cache.get(i).toString());
                 for (int j = 0; j < db.size(); j++) {
                     if (db.get(j).getIdtimetable()==cache.get(i).getIdtimetable())
                     {
@@ -65,7 +66,7 @@ public class TimetableChangeNotifier {
         if (db.size()!=0)
         {
             for (int i = 0; i < db.size(); i++) {
-                ChangeList.add("Добавлен урок "+db.get(i).toString());
+                ChangeList.add(R.string.ChangeList_LessonAdd+db.get(i).toString());
             }
         }
     }
@@ -79,10 +80,10 @@ public class TimetableChangeNotifier {
         for (int i = 0; i < cache.size(); i++) {
             if (DataBaseOperation.GetSubjectById(cache.get(i).getIdsubject()) == null)
             {
-                ChangeList.add("Удален предмет "+cache.get(i).getName());
+                ChangeList.add(R.string.ChangeList_SubjectDelete+cache.get(i).getName());
             }else if(!DataBaseOperation.GetSubjectById(cache.get(i).getIdsubject()).equals(cache.get(i)))
             {
-                ChangeList.add("Изменен предмет "+cache.get(i).getName());
+                ChangeList.add(R.string.ChangeList_SubjectUpdate+cache.get(i).getName());
                 for (int j = 0; j < db.size(); j++) {
                     if (db.get(j).getIdsubject()==cache.get(i).getIdsubject())
                     {
@@ -104,7 +105,7 @@ public class TimetableChangeNotifier {
         if (db.size()!=0)
         {
             for (int i = 0; i < db.size(); i++) {
-                ChangeList.add("Добавлен предмет "+db.get(i).getName());
+                ChangeList.add(R.string.ChangeList_SubjectAdd+db.get(i).getName());
             }
         }
 
@@ -115,17 +116,14 @@ public class TimetableChangeNotifier {
         List<Teacher> cache = Cache.ReadTeachers();
 
         List<Teacher> db = (List<Teacher>) DataBase.Teachers.clone();
-        debug.log("dbSIZE",db.size());
-
-        debug.log("cacheteavher",cache.get(0).getName());
 
         for (int i = 0; i < cache.size(); i++) {
             if (DataBaseOperation.GetTeacherById(cache.get(i).getIdTeacher()) == null)
             {
-                ChangeList.add("Удален преподаватель "+cache.get(i).getName());
+                ChangeList.add(R.string.ChangeList_TeacherDelete+cache.get(i).getName());
             }else if(!DataBaseOperation.GetTeacherById(cache.get(i).getIdTeacher()).equals(cache.get(i)))
             {
-                ChangeList.add("Изменен преподаватель "+cache.get(i).getName());
+                ChangeList.add(R.string.ChangeList_TeacherUpdate+cache.get(i).getName());
                 for (int j = 0; j < db.size(); j++) {
                     if (db.get(j).getIdTeacher()==cache.get(i).getIdTeacher())
                     {
@@ -147,7 +145,7 @@ public class TimetableChangeNotifier {
         if (db.size()!=0)
         {
             for (int i = 0; i < db.size(); i++) {
-                ChangeList.add("Добавлен преподаватель "+db.get(i).getName());
+                ChangeList.add(R.string.ChangeList_TeacherAdd+db.get(i).getName());
             }
         }
     }
@@ -160,7 +158,7 @@ public class TimetableChangeNotifier {
 
         if (!cache.get(cache.size()-1).equals(db.get(db.size()-1)))
         {
-            ChangeList.add("Добавлена новая неделя "+db.get(db.size()-1).toString());
+            ChangeList.add(R.string.ChangeList_WeekAdd+db.get(db.size()-1).toString());
             return true;
         }
         return false;
